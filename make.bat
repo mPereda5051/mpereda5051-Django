3a8773ea824af68de1f0bcf9f0e292c596cfdae7
Miguel <mpereda5051@gmail.com>
2026-02-17 12:11:43 +0100

    añadido cambios

--- make.bat
+++ make.bat
@@ -24,10 +24,10 @@ if "%command%" == "build" (
     docker-compose run --rm servidor bash
 ) else if "%command%" == "lint" (
     echo "Running linter with ruff..."
-    ruff check .
+    py -m ruff check .
 ) else if "%command%" == "lint-fix" (
     echo "Running linter with ruff and fixing errors..."
-    ruff check --fix .
+    py -m ruff check --fix .
 ) else (
     echo "Unknown command: %command%"
     echo Usage: make.bat [build^|up^|down^|logs^|shell^|lint^|lint-fix]
